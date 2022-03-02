cd3983337165ed14dfdfbbc35c2a96985d414e4d
stardust126 <56388847+stardust126@users.noreply.github.com>
2022-03-02 09:35:36 +0800

    Update apps.js

--- test/force-app/lwc/apps/apps/apps.js
+++ test/force-app/lwc/apps/apps/apps.js
@@ -1,11 +1,9 @@
 import { api, LightningElement,wire } from 'lwc';
 import getSurvey from '@salesforce/apex/survey.getSurvey';
 import getOwnerName from '@salesforce/apex/survey.getOwnerName';
-// import times from '@salesforce/apex/survey.times';
 import CURRENTUSERID from '@salesforce/user/Id';
 
 const columns = [
-    // {label: 'Id', fieldName: 'Id'},
     {label: 'Name', fieldName: 'Name'},
     {label: 'Catogery1', fieldName: 'Catogery1__c'},
     {label: 'Catogery2', fieldName: 'Catogery2__c'},
@@ -15,24 +13,15 @@ const columns = [
     {label: 'Catogery6', fieldName: 'Catogery6__c'},
     {label: 'Catogery7', fieldName: 'Catogery7__c'},
     {label: 'Catogery8', fieldName: 'Catogery8__c'},
-    // {label: 'Catogery9', fieldName: 'Catogery9__c'},
     {label: 'Catogery10', fieldName: 'Catogery10__c'},
 ];
 
 export default class Apps extends LightningElement {
     @wire(getSurvey) data;
     @wire(getOwnerName,{ids:'$currentUserId'}) OwnNameData;
-    // @wire(times) timed;
-    timed = new Date().getFullYear()+ '年' + (new Date().getUTCMonth() + 1)+ '月' + (new Date().getUTCDay() - 1) + '日' + ' ' + (new Date().getUTCHours() + 8) + ':' + new Date().getUTCMinutes();
+    timed = new Date().toLocaleString();
     columns = columns;
     currentUserId = CURRENTUSERID;
-
-    // time(){
-         
-    //     console.log(1);
-        
-    //     // console.log(timed.data);
-    // }
 
     handleClick(){
         console.log(11);
