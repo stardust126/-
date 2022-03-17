56005177ac6d3a016cdabaf0d2d11bc6bb1ecb18
stardust126 <56388847+stardust126@users.noreply.github.com>
2022-03-17 17:02:59 +0800

    Update showQuestions01.js

--- lwc/ShowQuestions01/showQuestions01.js
+++ lwc/ShowQuestions01/showQuestions01.js
@@ -3,4 +3,19 @@ import findQuestions from '@salesforce/apex/questionController.findQuestions';
 
 export default class ShowQuestions01 extends LightningElement {
     @wire(findQuestions) questions;
+    selectedNo;
+    
+    handler(evt){
+        this.selectedNo=evt.target.name;
+        this[NavigationMixin.Navigate]({
+            type: 'standard__component',
+            attributes: {
+                componentName: 'c__demo1'
+            },
+            state: {
+                c__No: this.selectedNo
+            }
+        });
+       
+    }
 }
